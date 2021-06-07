ae76a5f39cb8a23fbde839bba9ca324877aaa6c5
chris k <yovirinc@gmail.com>
2021-06-07 17:04:33 +0200

    header and intro done

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// mobile hamburger menu slide
+const mobileMenuOpenBtn = document.querySelector('#mobile-menu-open')
+const mobileMenuCloseBtn = document.querySelector('#mobile-menu-close')
+const mobileMenu = document.querySelector('#mobile-nav')
+const menuLinks = mobileMenu.querySelectorAll('.mobile-link')
+
+function mobileMenuOpen() {
+  mobileMenu.classList.add('opened')
+  menuLinks.forEach((link, i) => {
+    link.style.opacity = 1
+    link.style.transition = `opacity ${(i / 4) * 1.5}s`
+  })
+}
+
+function mobileMenuClose() {
+  mobileMenu.classList.remove('opened')
+  menuLinks.forEach((link, i) => {
+    link.style.opacity = 0
+  })
+}
+
+if (mobileMenuOpenBtn) {
+  mobileMenuOpenBtn.addEventListener('click', mobileMenuOpen)
+}
+
+if (mobileMenuCloseBtn) {
+  mobileMenuCloseBtn.addEventListener('click', mobileMenuClose)
+}
